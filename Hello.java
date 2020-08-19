7b1bddfcd6c64ec86db69e2c37479099977b2137
butlertx <69908919+butlertx@users.noreply.github.com>
2020-08-19 11:07:50 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************
+ * Hello.java
+ ************************************************************* Terran Butler
+ * 8/19/2020
+ * CMSC 255-003
+ ***********************************************************/
+
+public class Hello { 
+
+    public static void main(String[] args) {
+
+      System.out.println("<Terran Butler>");
+      System.out.println("<Lab 1>");
+      System.out.println("<First Java Program>");
+      System.out.println("<8/19/2020>");
+      System.out.println("<CMSC 255-003>");
+      System.out.println();
+      }                                                      
+ } 
+ 
