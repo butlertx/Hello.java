eeece7f6c650b441a3bd02f0e798a4023b09fb4c
butlertx <69908919+butlertx@users.noreply.github.com>
2020-08-27 16:40:24 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,34 @@
+/********************************************************************
+* NameArt.Java
+*********************************************************************
+* This project outputs my name in a unique way on the display screen
+*____________________________________________________________________
+* Terran Butler
+* Aug 26,2020
+* CMSC 255 003 Section
+*********************************************************************/
+
+public class NameArt
+{
+   public static void main(String[] args)
+   {
+   	 System.out.println(" Terran Butler ");    
+   	 System.out.println(" Project 1- Name in Code ");    
+   	 System.out.println(" This Project outputs my name in a unique way on the display screen ");    
+   	 System.out.println(" Aug 26, 2020");    
+   	 System.out.println(" CMSC 255 Section ");    
+   	 System.out.println(" ");    
+   	 System.out.println(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ");    
+   	 System.out.println(" @|                                                                                           |@ ");    
+   	 System.out.println(" @|        TBTBTBTBTB   TBTBTBTB   TBTBTBTB     TBTBTBTB           TB        TB        TB     |@ ");    
+   	 System.out.println(" @|            TB       TB         TB      TB   TB      TB       TB  TB      TBTB      TB     |@ ");    
+   	 System.out.println(" @|            TB       TBTBTBTB   TBTBTBTB     TBTBTBTB        TB    TB     TB  TB    TB     |@ ");    
+   	 System.out.println(" @|            TB       TB         TB  TB       TB  TB         TBTBTBTBTB    TB    TB  TB     |@ ");    
+   	 System.out.println(" @|            TB       TB         TB    TB     TB    TB      TB        TB   TB      TBTB     |@ ");    
+   	 System.out.println(" @|            TB       TBTBTBTB   TB      TB   TB      TB   TB          TB  TB        TB     |@ ");    
+   	 System.out.println(" @|                                                                                           |@ ");    
+   	 System.out.println(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ");
+
+
+   }
+}
